ff04d2f4bb397f14ad6b49223f265fef25dd56ff
Fabricio <fabricioalvesfragoso@gmail.com>
2024-10-08 19:04:06 -0300

    GeradorDeSenhas

---
+++ src/main/java/com/mycompany/geradordesenhas/GeradorDeSenhas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.geradordesenhas;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class GeradorDeSenhas {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();    
+        form.setVisible(true);
+    }
+}
